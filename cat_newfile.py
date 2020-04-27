aa208421fc55c3bdcc4fdc563865081b111df4c9
Taylor Higgins <higgins.taylor.a@gmail.com>
2020-04-27 02:24:08 -0400

    finished safe unsafe keyword seg tabs
    still need to get the last tab finished

--- cat_newfile.py
+++ cat_newfile.py
@@ -69,29 +69,19 @@ def standard_safety():
     gv_unsafe_threshold = 15.00
     sheet_dictionary = {}
     total_row_count = cat_sheet.max_row
-    i=0    
-    #see if the list row thing can make it easier to do priority 3 and 1...
-    #try it on all rows
-    for row in cat_sheet.iter_rows(min_col=1, min_row=2, max_col=40, max_row=40): #cat_sheet.max_row
-        sheet_dictionary[i] = {}
-        list_row = list(row)
-        #print(list_row) #cell value 
-        #print(type(list_row)) #list 
-        #print(type(list_row[5].value)) string
-        #print(list_row[5].value) keywords
-        for i in list_row:
-            #print(i) #prints all cells in the row 
-            #print(i.value) #prints cell values of row
-            try:
-                if "gv_" in i.value:
-                    #check_score = i + 2
-                    #print(check_score.value)
-                    print("unsafe")
-                    print(i)
-                    print(i.value.index)
-                    print(i.value)
-            except TypeError:
-                continue
+    
+    # for row in cat_sheet.iter_rows(min_col=1, min_row=2, max_col=40, max_row=40): #cat_sheet.max_row
+    #     #sheet_dictionary[i] = {}
+    #     #this list thing works!!! TODO use this for smart gv stuff 
+    #     list_row = list(row)
+    #     #print(list_row) #cell value 
+    #     # print(type(list_row)) #list 
+    #     # print(type(list_row[5].value)) string
+    #     # print(list_row[5].value) keywords
+    #     for item in list_row:
+    #         sheet_dictionary[item.row] = 
+
+
         # for cell in row:
         #     try:
         #         sheet_dictionary[i]["url"] = row[0].value
@@ -104,8 +94,8 @@ def standard_safety():
         #         continue
         #i+=1
 
-    gs_list = ['gs_entertain_movies', 'gs_entertain_vidgames', 'gs_entertain_tv', 'gs_society', 'gs_tech', 'gs_tech_phones', 'gs_home', 'gs_tech_computing']
-    gv_list = ['gv_crime']
+    # gs_list = ['gs_entertain_movies', 'gs_entertain_vidgames', 'gs_entertain_tv', 'gs_society', 'gs_tech', 'gs_tech_phones', 'gs_home', 'gs_tech_computing']
+    # gv_list = ['gv_crime']
 
     #TODO work on figuring out urls/rows that aren't safe, but don't have gv, null
     #go to file and see how many scores are more than 15.00
@@ -185,7 +175,7 @@ def standard_safety():
         #         continue
 
     #Making things simpler, use old logic with new caveat of threshold
-    row_count = cat_sheet.max_row 
+    #row_count = cat_sheet.max_row 
     #total_cat_urls = row_count - 1 - null_total
     #safe_total = total_cat_urls - unsafe_total
 
@@ -199,10 +189,10 @@ def standard_safety():
     #print(sheet_dictionary[9]) #should be unsafe good 
     #print(sheet_dictionary[21]) #will be safe
 
-
+    #print(sheet_dictionary)
     return(sheet_dictionary)
 
-def count_keywords_segments(sheet_dictionary):
+def count_keywords_segments():
     """TODO in its own tab for each i'll write the row of each dict that holds the total count"""
     #go through each row/url dict in the sheet_dict
     #if the row dict's key verdict value starts with safe
@@ -218,6 +208,87 @@ def count_keywords_segments(sheet_dictionary):
     #print(sheet_dictionary[5])
     #print(type(sheet_dictionary[5]["safety_boolean"]))
     #unsafe_segment_count_sheet.append()
+    safe_keyword_dict = {}
+    unsafe_keyword_dict = {}
+    #i=0    
+    #see if the list row thing can make it easier to do priority 3 and 1...
+    #try it on all rows
+    for row in cat_sheet.iter_rows(min_col=1, min_row=2, max_col=40, max_row=cat_sheet.max_row): #cat_sheet.max_row
+        #sheet_dictionary[i] = {}
+        #this list thing works!!! TODO use this for smart gv stuff 
+        list_row = list(row)
+        #print(list_row) #cell value 
+        # print(type(list_row)) #list 
+        # print(type(list_row[5].value)) string
+        # print(list_row[5].value) keywords
+        for item in list_row: #change i to item or cell
+            #print(item)
+            #print(item.value) #prints all cells in the row 
+            #print(i.value) #prints cell values of row
+            try:
+                #working on keywords tabs
+                #let's find safe unsafe, add whatever to get keywords
+                #then make a safe and unsafe keyword dict from it and count
+                #strip value before so unique
+
+                if "gv_" in item.value:
+                    #check_score = i + 2
+                    #print(item.value)
+                    check_keyword = item.col_idx
+                    unsafe_keyword_raw = list_row[check_keyword].value
+                    unsafe_keyword_clean = (unsafe_keyword_raw.split('|'))
+                    unsafe_keyword_clean.pop()
+                    unsafe_keyword_clean.pop(0)
+                    for unsafe_keyword in unsafe_keyword_clean:
+                        if unsafe_keyword in unsafe_keyword_dict:
+                            unsafe_keyword_dict[unsafe_keyword] +=1
+                        else:
+                            unsafe_keyword_dict[unsafe_keyword] = 1
+                    #print(check_keyword.value)
+                    #print(check_score.value)
+                    # print("unsafe")
+                    # print(i)
+                    # print(i.value.index)
+                    # print(i.value)
+                elif "gs_" in item.value:
+                    #print(item.value)
+                    #print(item.col_idx) #prints column number 
+                    #print(item) #prints cell object
+                    #print(item.row) #prints row number
+                    check_keyword = item.col_idx #should be plus 1 ... but does score not keyword... 
+                    #print(check_keyword)
+                    #print(list_row[check_keyword].value) #.value?
+                    safe_keyword_raw = list_row[check_keyword].value
+                    #safe_keyword_clean = (safe_keyword_raw.replace('|', ' ')).split() # '|'
+                    safe_keyword_clean = (safe_keyword_raw.split('|'))
+                    #print(safe_keyword_clean)
+                    safe_keyword_clean.pop()
+                    safe_keyword_clean.pop(0)
+                    #print(safe_keyword_clean)
+
+                    #treat safe_keyword_clean as a list now... 
+                    for safe_keyword in safe_keyword_clean:
+                        #print(safe_keyword)
+                        if safe_keyword in safe_keyword_dict:
+                            safe_keyword_dict[safe_keyword] +=1
+                        else:
+                            safe_keyword_dict[safe_keyword] = 1
+                    #print(item + 1)
+                    #print(check_keyword.value)
+                    #how do we handle not overcounting per row? break doesn't seem to be an option
+            
+            except TypeError:
+                continue
+    #print((safe_keyword_dict)) #unsorted  
+    #print(unsafe_keyword_dict)       
+    sorted_safe_keyword_dict = dict(sorted(safe_keyword_dict.items(), key=operator.itemgetter(1), reverse=True))
+    sorted_unsafe_keyword_dict = dict(sorted(unsafe_keyword_dict.items(), key=operator.itemgetter(1), reverse=True))   
+    #print(sorted_safe_keyword_dict)
+    safe_keyword_list = list(sorted_safe_keyword_dict.items())
+    unsafe_keyword_list = list(sorted_unsafe_keyword_dict.items())
+
+    return(safe_keyword_list, unsafe_keyword_list)
+
 
 def url_totals(sheet_dictionary):
 
@@ -232,6 +303,7 @@ def url_totals(sheet_dictionary):
     actual_safe_total = 0
     fake_total = 0
     safe_segment_dict = {}
+    unsafe_segment_dict = {}
     section_dict = {}
 ###NEW STUFF###
     new_unsafe_total = 0
@@ -296,19 +368,23 @@ def url_totals(sheet_dictionary):
                     fake_total += 1
                     break
                 elif check_safety is not None: 
+                    new_segment = cell.value
                     #print(cell)
                     #keyword_of_segment = (cell + 1).value #if this could work somehow...
                     #print(cell + 1) # or <Cell 'Sheet1'.H3984> edit the string so it's <Cell 'Sheet1'.H3985>
                     #print(cell.value)
                     #print(row[0]) #to check if safety is accurate
                     unsafe_total +=1 
+                    if new_segment in unsafe_segment_dict:
+                        unsafe_segment_dict[new_segment] = unsafe_segment_dict[new_segment] + 1
+                    else:
+                        unsafe_segment_dict[new_segment] = 1
                     #We put a break here so that we don't over count URLs that were categorized multiple times as unsafe.
                     break 
 
             except TypeError:
                 #This should pass from the blank cell to the next in the row, then finally to next URL row.
                 pass 
-
 #TODO look into this and see if it's counting unique urls or not, and if the chart 4 is accurate
     #Starting loop to create a dict of absolute count of segment appearances regardless of unique URL row. 
 #UPDATE I think by removing the break after else FIXED IT
@@ -330,11 +406,14 @@ def url_totals(sheet_dictionary):
                     #break #adding this break to see if we can get an accurate gs_ count
             except TypeError:
                 pass
-
+    #print(unsafe_segment_dict)
+    sorted_unsafe_seg_dict = dict(sorted(unsafe_segment_dict.items(), key=operator.itemgetter(1), reverse=True))
     #Here we are sorting the dict of safe segments in descending order by the value of the dict.   
     sorted_safe_seg_dict = dict(sorted(safe_segment_dict.items(), key=operator.itemgetter(1), reverse=True))
     #print(safe_segment_dict)
     #print(sorted_safe_seg_dict)
+    unsafe_segment_list = list(sorted_unsafe_seg_dict.items())
+    #print(unsafe_segment_list)
     #Here we are turning the sorted dict into a list so we can later append it by row and create the segment bar chart.
     safe_segment_list = list(sorted_safe_seg_dict.items())
     #print(sorted_safe_seg_dict)
@@ -358,12 +437,12 @@ def url_totals(sheet_dictionary):
 
     #Here we are sorting the dict of sections in descending order by the value of the dict.
     sorted_section_dict = dict(sorted(section_dict.items(), key=operator.itemgetter(1), reverse=True))
-
+    #print(sorted_section_dict)
     #print(sorted_section_dict)
     #Here we are turning the sorted dict into a list so we can later append it by row to the file. 
     section_list = list(sorted_section_dict.items())
-    
-    
+    #print(section_list) this is fine
+
     #Calculate total categorized URLs. 
     row_count = cat_sheet.max_row 
     total_cat_urls = row_count - 1 - null_total
@@ -376,16 +455,17 @@ def url_totals(sheet_dictionary):
     # print("null total is {}".format(null_total))
     # print("unsafe total is {}".format(unsafe_total))
     # print("safe total is {}".format(safe_total))
-
-    return(null_total, unsafe_total, safe_total, safe_segment_list, section_list)
+    
+    return(null_total, unsafe_total, safe_total, safe_segment_list, section_list, unsafe_segment_list)
 
 def section_many_segments(section_list):
     """Create the Incremental Inventory Bar Chart"""
     #Here we pick the most popular section from the section list. 
     #Note that we might end up picking the top 3 to give more options.
-    
+    #print(section_list) #this is fine
     most_popular_section = section_list[2][0] #do the top 3
-    #print(section_list[2][0])
+    #print(section_list[2][0]) #this gives me gv_adult FIXED YAY
+    #print(most_popular_section) #this is fine
     hardchart1_sheet['B1'] = most_popular_section
     popular_segment_count = {}
 
@@ -570,6 +650,70 @@ def popular_bar_chart(safe_segment_list):
     bar.title = 'Total Appearances'
     charts_sheet.add_chart(bar, "H20")
 
+def unsafe_segment_count(unsafe_segment_list):
+    """somehow I made a bug using sections here, it affects the section chart as well"""
+    for row in unsafe_segment_list:
+        unsafe_segment_count_sheet.append(row)
+
+def safe_segment_count(safe_segment_list):
+    for row in safe_segment_list:
+        safe_segment_count_sheet.append(row)
+
+def unsafe_keyword_count(unsafe_keyword_list):
+    for row in unsafe_keyword_list:
+        unsafe_keywords_count_sheet.append(row)
+
+def safe_keyword_count(safe_keyword_list):
+    for row in safe_keyword_list:
+        safe_keywords_count_sheet.append(row)
+
+def url_safe_unsafe(sheet_dictionary):
+    """write the dictionary to each row of this tab,
+    want to loop through each row dict and print it to
+    the row hwere"""
+    url_safe_unsafe_sheet['A2'] = "Placeholder tab, not finished yet! "
+
+
+    # #or try this
+    # for inner_dict in sheet_dictionary:
+    #     for row in url_safe_unsafe_sheet.rows:
+    #         row.value = sheet_dictionary[inner_dict]
+
+    # for inner_dict in sheet_dictionary:
+    #     #this sheet_dictionary has 1{url:x,seg:x,key:x,score:x}
+    #     #print(row) is the numbers
+    #     print(type(sheet_dictionary[inner_dict])) #this is the entire dictionary 
+    #     #url_safe_unsafe_sheet.append(sheet_dictionary[inner_dict])
+    #     #this doesn't work! 
+    #     #url_safe_unsafe_sheet.append(sheet_dictionary[inner_dict])
+        
+    #     for item in sheet_dictionary[inner_dict]:
+
+    #         #print(sheet_dictionary[row][i]) #each individual value or list from each inner dict
+    #         #below kinda works, not ideal but it writes each value underneath each and since we made url a list it separates the letters stupidly
+    #         #list_version = list(sheet_dictionary[row][i]) # url and boolean are strings, could do an if type not list then apply list but let's try this first 
+    #         #url_safe_unsafe_sheet.append(list_version)
+    #         #what happens when I do this? I want each item in the inner dict to appear in its own column, like url, segment list, keyword list etc
+    #         #print(type(item)) #string
+    #         #list_version = list(sheet_dictionary[inner_dict])
+    #         #other_list_version = list(sheet_dictionary[inner_dict][item])
+    #         #url_safe_unsafe_sheet.append(other_list_version)
+    #         #url_safe_unsafe_sheet.append(item)
+
+    #         #print(type(sheet_dictionary[inner_dict][item]))
+    #         #print(sheet_dictionary[inner_dict][item])
+    #         for row in  url_safe_unsafe_sheet.rows:
+    #             row.value = sheet_dictionary[inner_dict][item]
+    #         #url_safe_unsafe_sheet.append(dict(sheet_dictionary[inner_dict][item]))
+    
+    #     #I should just be using csv module but converting it and don't understand well enough
+#         import csv
+
+# with open('output.csv', 'wb') as output:
+#     writer = csv.writer(output)
+#     for key, value in dict1.iteritems():
+#         writer.writerow([key, value])
+    #     #I also don't understand teh append or cell() methods well enough for openpyxl
 
 if __name__ == '__main__': 
 
@@ -591,9 +735,10 @@ if __name__ == '__main__':
     workbook.create_sheet('Chart 2')
     workbook.create_sheet('Chart 3 and 4')
     workbook.create_sheet('Unsafe Segment Count')
-    workbook.create_sheet('Unsafe Keywords Count')
     workbook.create_sheet('Safe Segment Count')
+    workbook.create_sheet('Unsafe Keywords Count')
     workbook.create_sheet('Safe Keywords Count')
+    workbook.create_sheet('URL Safe Unsafe')
 
     #Create new sheets for sections and charts and make variables of each sheet we'll need. 
     cat_sheet = workbook['Sheet1']
@@ -602,9 +747,13 @@ if __name__ == '__main__':
     hardchart2_sheet = workbook['Chart 2']
     charts_sheet = workbook['Chart 3 and 4']
     unsafe_segment_count_sheet = workbook['Unsafe Segment Count']
-    unsafe_keywords_count_sheet = workbook['Unsafe Keywords Count']
     safe_segment_count_sheet = workbook['Safe Segment Count']
+
+    #odd these are being switched somehow... TODO
+    unsafe_keywords_count_sheet = workbook['Unsafe Keywords Count']
     safe_keywords_count_sheet = workbook['Safe Keywords Count']
+
+    url_safe_unsafe_sheet = workbook['URL Safe Unsafe']
 
     #Add in headers and clean up the categorized sheet.
     cat_sheet.delete_cols(2)
@@ -630,21 +779,33 @@ if __name__ == '__main__':
     hardchart2_sheet['A3'] = "Sections"
     hardchart2_sheet['B3'] = "Count"
     unsafe_segment_count_sheet['A1'] = "Segment"
-    unsafe_segment_count_sheet['A2'] = "Count"
-    unsafe_keywords_count_sheet['A1'] = "Keywords"
-    unsafe_keywords_count_sheet['A2'] = "Count"
+    unsafe_segment_count_sheet['B1'] = "Count"
     safe_segment_count_sheet['A1'] = "Segment"
-    safe_segment_count_sheet['A2'] = "Count"
-    safe_keywords_count_sheet['A1'] = "Keywords"
-    safe_keywords_count_sheet['A2'] = "Count"
+    safe_segment_count_sheet['B1'] = "Count"
 
+    unsafe_keywords_count_sheet['A1'] = "Keywords"
+    unsafe_keywords_count_sheet['B1'] = "Count"
+
+    safe_keywords_count_sheet['A1'] = "Keywords"
+    safe_keywords_count_sheet['B1'] = "Count"
+
+    url_safe_unsafe_sheet['A1'] = "URL"
+    url_safe_unsafe_sheet['B1'] =  "URL Safety"
+    url_safe_unsafe_sheet['C1'] = "Segments"
+    url_safe_unsafe_sheet['D1'] = "Keywords"
+    url_safe_unsafe_sheet['E1'] = "Scores"
     (http_true) = url_path_parser()
     (sheet_dictionary) = standard_safety()
-    (null_total,unsafe_total, safe_total, safe_segment_list, section_list) = url_totals(sheet_dictionary)
-    count_keywords_segments(sheet_dictionary)
+    (null_total,unsafe_total, safe_total, safe_segment_list, section_list, unsafe_segment_list) = url_totals(sheet_dictionary)
+    (unsafe_keyword_list, safe_keyword_list) = count_keywords_segments()
     segment_many_sections(safe_segment_list, http_true)
     section_many_segments(section_list)
     safe_pie_chart(null_total,unsafe_total, safe_total)
     popular_bar_chart(safe_segment_list)
+    unsafe_segment_count(unsafe_segment_list)
+    safe_segment_count(safe_segment_list)
+    unsafe_keyword_count(unsafe_keyword_list)
+    safe_keyword_count(safe_keyword_list)
+    url_safe_unsafe(sheet_dictionary)
     workbook.save(filename="{}{}{}".format("charts_",shorter_name,".xlsx"))
     print("Using input file named {} the context script has finished!".format(name))
